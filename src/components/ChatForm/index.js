e966fa943d762d2ed7ece30e3c35a8e6a2f08f75
Dmitry Zaitsev <d.zaitsev@corp.mail.ru>
2025-10-30 20:40:04 +0300

    my-super-chat: wip

--- src/components/ChatForm/index.js
+++ src/components/ChatForm/index.js
@@ -6,3 +6,30 @@ export const ChatFormComponent = () => {
     </form>
   `
 }
+
+let formUnlistenMap = [] // [form, clb]
+
+const UnlistenFormSubmit = () => {
+  if (!formUnlistenMap.length) {
+    return;
+  }
+
+  const [formElement, listenerCallback] = formUnlistenMap;
+  formElement.removeEventListener('submit', listenerCallback)
+  formUnlistenMap = [];
+}
+
+export const ListenFormSubmit = (formElement, callback) => {
+  UnlistenFormSubmit();
+
+  elementWithChats.querySelectorAll('a').forEach((a) => {
+    const listenerCallback = (e) => {
+      e.preventDefault();
+      const clickedIndex = a.href.split('/').at(-1);
+      callback(+clickedIndex)
+    }
+
+    elPool.push([a, listenerCallback])
+    a.addEventListener('click', listenerCallback);
+  })
+}
