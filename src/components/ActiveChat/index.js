4258cbf3d518f0ee7df3d7284ebefcedea5b1f79
Dmitry Zaitsev <d.zaitsev@corp.mail.ru>
2025-10-30 20:14:57 +0300

    my-super-chat: wip

--- src/components/ActiveChat/index.js
+++ src/components/ActiveChat/index.js
@@ -1,5 +1,23 @@
-export const ActiveChatComponent = (id) => {
+import './style.css'
+
+const Message = (text, time, isMy) => {
+  const isMyClass = isMy ? 'ac-message_right' : 'ac-message_left'
   return `
-    active chat: ${id}
+    <div class="ac-message ${isMyClass}">
+      <div class="ac-text">${text}</div>
+      <div class="ac-time">${time}</div>
+    </div>
+  `
+}
+
+export const ActiveChatComponent = (id) => {
+  const msg1 = Message('Privet!', '20:10', true)
+  const msg2 = Message('Poka!', '20:20', false)
+
+  return `
+    <div class="ac-container">
+      ${msg1}
+      ${msg2}
+    </div>
   `
 }
