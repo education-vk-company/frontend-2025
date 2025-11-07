b32ed9021a4ca63dcb7ff52faad6d078f945584f
Martin Komitsky <m.komitsky@corp.mail.ru>
2025-11-07 19:05:32 +0300

    my-super-chat-react: added classnames

--- src/components/ActiveMessage/ActiveMessage.jsx
+++ src/components/ActiveMessage/ActiveMessage.jsx
@@ -1,10 +1,11 @@
 import styles from './ActiveMessage.module.css'
+import classNames from 'classnames'
 
 export const ActiveMessage = ({ isMy, text, time }) => {
 
   return (
     <div className={styles.acWrapper}>
-      <div className={styles.acMessage}>
+      <div className={classNames(styles.acMessage, styles.acMessage_right)}>
         <div className={styles.acText}>${text}</div>
         <div className={styles.acTime}>${time}</div>
       </div>
