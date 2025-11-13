adf61c6d119781ac9361e8e472283ae6ed72193c
Dmitry Zaitsev <d.zaitsev@corp.mail.ru>
2025-11-13 20:32:59 +0300

    l6-spa-reactrouter: added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,32 @@
-import { useEffect, useRef, useState } from 'react'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 import { AboutPage } from './pages/About'
 import { ContactsPage } from './pages/Contacts'
 import { LoginPage } from './pages/Login'
 
 function App() {
-
-  const [page, setPage] = useState(location.pathname.slice(1));
-  const headerRef = useRef(null);
-
-  useEffect(() => {
-    const Header = headerRef.current;
-    if (!Header) {
-      return;
-    }
-
-    const links = Header.querySelectorAll('a');
-    links.forEach(link => {
-      link.addEventListener('click', e => {
-        e.preventDefault();
-        setPage(link.pathname.slice(1))
-        history.pushState({}, '', link.href)
-      });
-    });
-
-    window.addEventListener('popstate', () => {
-      setPage(location.pathname.slice(1))
-    });
-  }, [])
-
   return (
     <div>
-      <header ref={headerRef}>
-        <ul>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contacts">Contacts</a>
-          </li>
-          <li>
-            <a href="/login">Login</a>
-          </li>
-        </ul>
-      </header>
-
-      { page === 'about' && <AboutPage />}
-      { page === 'contacts' && <ContactsPage />}
-      { page === 'login' && <LoginPage />}
+      <Router>
+        <header>
+          <ul>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contacts">Contacts</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </header>
+        <Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
