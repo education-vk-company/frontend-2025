cb3104e216ee11dd3b6858d285b529df804c04e7
Dmitry Zaitsev <d.zaitsev@corp.mail.ru>
2025-11-13 20:24:50 +0300

    l6-spa-ordinary: ordinary

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { AboutPage } from './pages/About'
 import { ContactsPage } from './pages/Contacts'
@@ -6,27 +6,41 @@ import { LoginPage } from './pages/Login'
 
 function App() {
 
-  const [page, setPage] = useState(location.hash.slice(1));
+  const [page, setPage] = useState(location.pathname.slice(1));
+  const headerRef = useRef(null);
 
   useEffect(() => {
-    window.addEventListener('hashchange', () => {
-      const pageFromHash = location.hash.slice(1)
-      setPage(pageFromHash)
+    const Header = headerRef.current;
+    if (!Header) {
+      return;
+    }
+
+    const links = Header.querySelectorAll('a');
+    links.forEach(link => {
+      link.addEventListener('click', e => {
+        e.preventDefault();
+        setPage(link.pathname.slice(1))
+        history.pushState({}, '', link.href)
+      });
+    });
+
+    window.addEventListener('popstate', () => {
+      setPage(location.pathname.slice(1))
     });
   }, [])
 
   return (
     <div>
-      <header>
+      <header ref={headerRef}>
         <ul>
           <li>
-            <a href="#about">About</a>
+            <a href="/about">About</a>
           </li>
           <li>
-            <a href="#contacts">Contacts</a>
+            <a href="/contacts">Contacts</a>
           </li>
           <li>
-            <a href="#login">Login</a>
+            <a href="/login">Login</a>
           </li>
         </ul>
       </header>
